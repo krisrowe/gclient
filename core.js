850c6dd5ceb480c42316991552dd51807cbd7e4e
Kris Rowe <kris@rowelink.com>
2023-01-31 00:20:10 -0600

    Provided enhancements to date utilities and better documentation.

--- core.js
+++ core.js
@@ -2,8 +2,20 @@ function isDate(s) {
     return (new Date(s) !== "Invalid Date") && !isNaN(new Date(s));
 }
 
+/**
+ * Returns a new Date object with the time set to 00:00:00.000
+ * @returns {Date} The date with the time set to 00:00:00.000
+ */
 Date.prototype.getWithoutTime = function () {
     return new Date(this.toDateString());
+}
+
+/**
+ * Returns a string in the format YYYY-MM-DD
+ * @returns {string} The date in the format YYYY-MM-DD
+ */
+Date.prototype.toISOStringWithoutTime = function () {
+    return this.toISOString().split('T')[0];
 }
 
 Date.prototype.addDays = function(days) {
@@ -12,10 +24,22 @@ Date.prototype.addDays = function(days) {
     return date;
 }
 
+/**
+ * Compares only the date portion of the two date objects, ignoring the time.
+ * @param {Date} otherDate The date to compare to.
+ * @returns {boolean} true if the dates are equal without considering the time.
+ */
 Date.prototype.dateEquals = function(otherDate) {
     return this.getWithoutTime().getTime() == otherDate.getWithoutTime().getTime();
 }
 
+/**
+ * Parses a date in the format YYYY-MM-DD and returns a Date object
+ * that represents that date with the time set to 00:00:00.000 in the
+ * local time zone.
+ * @param {*} s 
+ * @returns {Date} date with the time set to 00:00:00.000 in the local time zone.
+ */
 function parseIsoDate(s) {
   var pattern = /^(\d{4})-(\d{1,2})-(\d{1,2})$/;
   var segments = s.match(pattern);
@@ -44,9 +68,13 @@ function reversePropertyMap(map) {
   return result;
 }
 
-function sign(message){     
-  var signature = Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, 
-    message, Utilities.Charset.US_ASCII);
+function sign(message){   
+  // MD5 is a 128-bit hash, so we need to convert it to a 32-character hex string
+  // Compute a digest of the message using MD5.
+  var signature = require('crypto').createHash('md5').update(message).digest('hex');
+  
+  //var signature = Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, 
+  //  message, Utilities.Charset.US_ASCII);
   var signatureStr = '';
     for (i = 0; i < signature.length; i++) {
       var byte = signature[i];
@@ -140,4 +168,4 @@ function extractAmountField(sourceText, fieldLabel, reverse) {
     }
 }
 
-module.exports = { mapProperties, reversePropertyMap, extractSingleValue, extractAmountField, extractFieldLineValue, getAmountFieldRegEx, findDateRange, singleDateExp, sign };
+module.exports = { parseIsoDate, mapProperties, reversePropertyMap, extractSingleValue, extractAmountField, extractFieldLineValue, getAmountFieldRegEx, findDateRange, singleDateExp, sign };
---
+++ test/core.js
@@ -0,0 +1,29 @@
+const core = require('../core.js');
+const crypto = require('crypto');
+const assert = require('assert');
+
+describe ('core', function() {
+    it('parse ISO date', function() {
+        const ISO_DATE_STRING = '2022-12-30';
+        const date = core.parseIsoDate(ISO_DATE_STRING);
+        assert.equal(date.getFullYear(), 2022);
+        assert.equal(date.getMonth(), 11);
+        assert.equal(date.getDate(), 30);
+        assert.equal(date.getHours(), 0);
+        assert.equal(date.getMinutes(), 0);
+    });
+
+    it ('convert date to ISO string without time', function() {
+        const ISO_DATE_STRING = '2022-12-30';
+        const date = core.parseIsoDate(ISO_DATE_STRING);
+        const isoString = date.toISOStringWithoutTime();
+        assert.equal(isoString, ISO_DATE_STRING);
+    });
+
+    it('sign', function() {
+        var fingerprint = "1234" + "|" + '2021-12-01' + "|" + "-9.99" + "|" + 
+            "1" + "|" + "VENMO PAYMENT";
+        var sig = crypto.createHash('md5').update(fingerprint).digest('hex');
+        assert.equal(sig, '8ec08968f744f515d18f628204c15083');
+    });
+});
